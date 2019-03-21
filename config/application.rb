e127e0847f8991fff0f2c71da2ee28943c93a1a7
Salekin <sjsalekin@gmail.com>
2019-03-21 06:57:36 +0000

    User model, irbtools and awesome_print

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'hirb'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Hirb.enable({:width => 256, :height => 512})
 
 module Todo
   class Application < Rails::Application
